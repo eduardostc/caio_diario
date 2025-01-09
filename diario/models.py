a7996e013e53fcdcf22dfcbbd5cd49268d07d1e6
Carlos Eduardo Rodrigues dos Santos <cers.rodrigues@gmail.com>
2025-01-08 23:25:04 -0300

    criado superuser

--- diario/models.py
+++ diario/models.py
@@ -13,7 +13,7 @@ class Diario(models.Model):
   titulo = models.CharField(max_length=100)
   tags = models.TextField()
   texto = models.TextField()
-  pessoa = models.ManyToManyField(Pessoa)
+  pessoa = models.ManyToManyField(Pessoa, null=True, blank=True)
   create_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
